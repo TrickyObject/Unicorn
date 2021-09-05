48ad7ac616f74e4b36e4c9b82a4bd437fb5c92b2
TrickyObject <krasusworks@gmail.com>
2021-09-05 22:57:47 +0700

    Добавлен контрол-файл

---
+++ src/main/java/mts/support/validator/Controller.java
@@ -0,0 +1,18 @@
+package mts.support.validator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private void click(ActionEvent event) {
+        button.setText("You've clicked!");
+    }
+
+
+}

31cfe7329cfc12346e28a10b5dcdb629e4e9de70
TrickyObject <krasusworks@gmail.com>
2021-09-05 23:10:57 +0700

    Нужно разобраться с модулями и их доступами друг к другу

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module mts.support.validator {
+//    модулю нужны библиотеки
     requires javafx.controls;
     requires java.xml;
     requires javafx.fxml;
+//позволяет экспортировать модуль проекта в модуль плагина
     exports mts.support.validator;
+//открывает доступ плагина к содержимому модуля проекта
+    opens mts.support.validator to javafx.fxml;
 }
